60e32064ded3ae69954c584e48e12b2fc6018619
wanbing01 <wanbing01@58.com>
2025-04-21 10:46:50 +0800

    compiler publish

--- example/lib/src/plugin/fair_common_plugin.dart
+++ example/lib/src/plugin/fair_common_plugin.dart
@@ -1,7 +1,7 @@
 // 由 bin/fair_common_plugin.dart 生成
 import 'package:fair/fair.dart';
 
-/// 跟 js 交互的方法类
+/// 跟js交互的方法类
 class FairCommonPlugin extends IFairPlugin with CompleterPlugin {
   factory FairCommonPlugin() => _fairCommonPlugin;
   FairCommonPlugin._();
